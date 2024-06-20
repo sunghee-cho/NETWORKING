f69f4c7f3393075ef650ad480a37ac5b6d56e833
ssoony00 <superior0130@gmail.com>
2024-06-20 16:38:29 +0900

    6.20 2

--- src/components/LeftSideb.js
+++ src/components/LeftSideb.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import profileImage from '../assets/images/고양이 프로필.png';
 
 const LeftSideb = () => {
     return (
         <div className="leftsidebar">
             <div className="profile">
-                <img src="../assets/images/고양이 프로필.png" alt="Profile Picture" style={{ borderRadius: '50%' }} />
+                <img src={profileImage} alt="Profile Picture" style={{ borderRadius: '50%' }} />
                 <p style={{ fontWeight: 'bold' }}>사용자 이름</p>
                 <button className="button">설정</button>
             </div>
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,22 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import postImage from '../assets/images/피드 메이지 예시.png';
+import postprofileImage from '../assets/images/고양이 프로필.png';
 
 const Post = () => {
-    return (
-        <div className="post">
-            <img src="../assets/images/피드 메이지 예시.png" alt="Post Image" className="main-img" />
-            <div className="description">들어갈 내용</div>
-            <div className="meta">
-                <span>💬 2 comments</span>
-                <span className="like-count">5 likes</span>
+        //댓글 수
+        const [comments, setComments] = useState(0); //처음
+        const [likes, setLikes] = useState(5); //처음
+        const [commentText, setCommentText] = useState(''); 
+        const [commentList, setCommentList] = useState([]); 
+    
+      
+        const handleCommentChange = (event) => {
+            setCommentText(event.target.value);
+        };
+    
+        
+        const handleCommentSubmit = () => {
+            if (commentText.trim() !== '') {
+                setCommentList([...commentList, commentText]); 
+                setCommentText(''); 
+                setComments(comments + 1); 
+            }
+        };
+    
+        return (
+            <div className="post">
+                <img src={postImage} alt="Post Image" className="main-img" />
+                <div className="description">들어갈 내용</div>
+                <div className="meta">
+                    <span>💬 {comments} comments</span>
+                    <span className="like-count">{likes} likes</span>
+                </div>
+                <div className="comments">
+                    {commentList.map((comment, index) => (
+                        <div key={index} className="comment">{comment}</div>
+                    ))}
+                </div>
+                <div className="comment-input">
+                    <img src={postprofileImage} alt="User Profile" className="profile-pic" />
+                    <input
+                        type="text"
+                        placeholder="댓글을 입력하세요..."
+                        className="comment-textbox"
+                        value={commentText}
+                        onChange={handleCommentChange}
+                    />
+                    <button className="comment-button" onClick={handleCommentSubmit}>Post</button>
+                </div>
             </div>
-            <div className="comments"></div>
-            <div className="comment-input">
-                <img src="../assets/images/고양이 프로필.png" alt="User Profile" className="profile-pic" />
-                <input type="text" placeholder="댓글을 입력하세요..." className="comment-textbox" />
-                <button className="comment-button">Post</button>
-            </div>
-        </div>
-    );
+        );
 };
 
 export default Post;
