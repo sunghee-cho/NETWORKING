9cce3fc4028d9c12a58566f0db51131c00f79f89
ssoony00 <superior0130@gmail.com>
2024-07-01 13:52:19 +0900

    7.1 1

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import '../styles/css_social.css';
+import '../styles/Socialcss/css_social.css';
 
 const Header = () => {
     return (
-        <div className="header">
-            <div className="logo"><span style={{color: 'black'}}>NET</span>WORKING</div>
-            <div className="menu">
-                <a href="#">Page1</a>
-                <a href="#">Page2</a>
-                <a href="#">Page3</a>
-                <button className="button">Button</button>
-            </div>
+      <header className="header">
+        <div className="logo">
+          NET<span>WORKING</span>
         </div>
+        <nav>
+          <a href="/">홈</a>
+          <a href="/jobs">채용 공고</a>
+          <a href="/messages">메시지</a>
+          <a href="/notifications" className="active">
+            알림
+          </a>
+        </nav>
+      </header>
     );
-};
-
-export default Header;
+  };
+  
+  export default Header;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import './styles/css_social.css';
-import Social from './pages/Social'; 
+import './styles/Socialcss/css_social.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Social /> 
+    <App /> 
   </React.StrictMode>
 );
 
